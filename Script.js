cde088340ff8f9c7f475fc80cda3651765d99ba2
Brij Mohan <bmohan@tavisca.in>
2019-08-22 01:40:13 +0530

    update html, javascript and css file

--- Script.js
+++ Script.js
@@ -1,36 +1,37 @@
-function GetAutopopulateData(){
-    console.log(AutopopulateData);
-
-    for (var index = 0; index < AutopopulateData.length; ++index) {
-        var node = document.createElement("li");
-        var textnode = document.createTextNode(
-                AutopopulateData[index].id + " :- " + AutopopulateData[index].title
-            );
-        node.appendChild(textnode);
-        document.getElementById("autopopulate-div-ul").appendChild(node);
-    }
+window.onload = function () {
+    ShowItem();
 }
 
-function addItem(){
-    var input = document.getElementById("Input");
-    var node = document.createElement("LI");
-    var nodeItem = document.createTextNode(input.value);
+function AddItem(){
+	/*var addValue = document.getElementById("searchBar").value;
+	var list = document.getElementById("list");
+	var listItem = list.appendChild(document.createElement("li").appendChild(document.createTextNode(addValue)));
+	document.getElementById("content").appendChild(listItem);*/
+
+	let input = document.getElementById("searchBar");
+    let node = document.createElement("li");
+    let nodeItem = document.createTextNode(input.value);
     node.appendChild(nodeItem);
-    doccument.getElementById("List").appendChild(node);
-}	
-function searchItem() {
-    var a, txtValue;
-    var input = document.getElementById("Input");
-    var filter = input.value.toUpperCase();
-    var list = document.getElementById("List");
-    var listItem = list.getElementsByTagName("li");
-    for (var i = 0; i < listItem.length; i++) {
-        a = listItem[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            listItem[i].style.display = "";
-        } else {
-            listItem[i].style.display = "none";
-        }
+    document.getElementById("list").appendChild(node);
+}
+
+function ShowItem(event){
+	//console.log(event);
+	// IE does not know about the target attribute. It looks for srcElement
+    // This function will get the event target in a browser-compatible way
+    event = event || window.event;
+    let target = event.target || event.srcElement;
+
+    let showId = target.id + "-container";
+    let ContentIds = document.getElementsByClassName("show-hide-section");
+    //console.log(ContentIds);
+    for(let object=0;object<ContentIds.length;object++){
+    	//console.log(ContentIds[object].id);
+    	//console.log(showId);
+    	ContentIds[object].id == showId ? 
+    	document.getElementById(showId).style.display = "block" : 
+    	document.getElementById(ContentIds[object].id).style.display = "none"; 
     }
 }
+
+
